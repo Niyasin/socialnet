46053ae63142211d23fe0119bb3308904442a86f
Niyasin <mo.niyas2000@gmail.com>
2023-03-18 23:13:27 +0530

    Login/Signup Component

---
+++ client/src/components/Login.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function Login(){
+    const [account,setAccount]=useState(false);
+    const [username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
+    const [password2,setPassword2]=useState('');
+    return(
+        <div className="container">
+            <div className="main">
+                <h1>Discover,Connect<br/>Explore</h1>
+                <div className="form">
+                    {account?<>
+                        <h2>Login</h2>
+                        <input placeholder='Username' onChange={(e)=>{setUsername(e.target.value)}}/>
+                        <input placeholder='Password' type='password'  onChange={(e)=>{setPassword(e.target.value)}}/>
+                        <span className='link' onClick={()=>{setAccount(false)}}>Dont Have An Account ?</span>
+                        <div className="button">Login</div>
+                    </>:<>
+                        <h2>Signup</h2>
+                        <input placeholder='Username'  onChange={(e)=>{setUsername(e.target.value)}}/>
+                        <input placeholder='Password' type='password'  onChange={(e)=>{setPassword(e.target.value)}}/>
+                        <input placeholder='Confirm Password' type='password' onChange={(e)=>{setPassword2(e.target.value)}}/>
+                        <span className='link' onClick={()=>{setAccount(true)}} >Alredy Have An Account ?</span>
+                        <div className="button">Signup</div>
+                    </>}
+                </div>
+            </div>
+            <img src="./images/main.jpg" className='heroImg'/>
+        </div>
+    )
+}
