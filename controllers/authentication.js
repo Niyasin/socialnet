47b5319a773ec8c53e79a1b1042600cf8cc70779
Niyasin <mo.niyas2000@gmail.com>
2023-03-19 14:42:22 +0530

    jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,12 @@
 const User =require('../models/user');
 const {scryptSync,randomBytes} =require('crypto')
+const {sign} =require('jsonwebtoken');
+
+const appSecret='secret'
+
+const createToken=(id)=>{
+   return sign({id},appSecret,{expiresIn:60});
+}
 
 module.exports.signup=async (req,res)=>{
     let doc={
@@ -16,9 +23,10 @@ module.exports.signup=async (req,res)=>{
     doc.password=`${password}:${salt}`;
     try{
         let user = await User.create(doc);
+         doc.token=createToken(user._id);
+         res.setHeader('Set-Cookie',`token=${doc.token}`);
         res.send(JSON.stringify({error:false,data:doc}));
     }catch(e){
         res.send(JSON.stringify({error:true}));
     }
-
 }
